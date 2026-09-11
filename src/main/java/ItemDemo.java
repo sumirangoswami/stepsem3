668dd389ef1f79c9a7c7f018ddda0ca8f39e2530
sumirangoswami <dayalsumiran2006@gmail.com>
2026-09-12 02:27:43 +0530

    week 4

---
+++ src/main/java/ItemDemo.java
@@ -0,0 +1,33 @@
+class Item {
+    String itemName;
+    int stock;
+
+    public Item(String itemName, int stock) {
+        this.itemName = itemName;
+        this.stock = stock;
+    }
+
+    void restock(int stock) {
+        this.stock = this.stock + stock;
+    }
+
+    void printStock() {
+        System.out.println(itemName + " | Final Stock: " + stock);
+    }
+}
+
+public class ItemDemo {
+    public static void main(String[] args) {
+        Item[] items = {
+            new Item("Samosa", 15),
+            new Item("Tea Powder", 40),
+            new Item("Bread", 8),
+            new Item("Biscuit Packs", 25)
+        };
+
+        for (int i = 0; i < items.length; i++) {
+            items[i].restock(20);
+            items[i].printStock();
+        }
+    }
+}
